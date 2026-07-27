ead56861ae1c92c60def71dfc975167782ed6c31
DBA-AQUAA <78464789+DBA-AQUAA@users.noreply.github.com>
2026-07-27 10:25:46 -0600

    Add files via upload

---
+++ js/catalogos-generales.js
@@ -0,0 +1,48 @@
+window.SGSA_GENERAL_CATALOGS = Object.freeze({
+  areas: Object.freeze([
+    "Administración",
+    "ASEC",
+    "Agua y Alcantarillado",
+    "Contabilidad",
+    "Cultura del Agua",
+    "Facturación y Cobranza",
+    "Fuentes de Abastecimiento",
+    "Jurídico",
+    "Jurídico Comercial",
+    "Jurídico Contencioso",
+    "Planeación y Proyectos",
+    "Recursos Humanos",
+    "Saneamiento",
+    "Tesorería"
+  ]),
+
+  sucursales: Object.freeze([
+    "Bodega 19",
+    "Bodega 56",
+    "Bodega 70",
+    "El Refugio",
+    "La Pradera",
+    "Zakia",
+    "Zibatá"
+  ]),
+
+  unidades: Object.freeze([
+    "Litro",
+    "Pieza",
+    "Caja",
+    "Paquete",
+    "Par",
+    "Juego",
+    "Cubeta",
+    "Galón",
+    "Kilogramo",
+    "Metro",
+    "Rollo",
+    "Costal",
+    "Bulto",
+    "Botella",
+    "Lata",
+    "Sobre",
+    "Otro"
+  ])
+});
